d84a28c75ade67ce30966205b1050b6a5d9bd593
Keystone Alves <keystonealves@gmail.com>
2023-09-14 15:38:45 -0300

    add animation go to top button

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -23,7 +23,7 @@ const BackToTopButton = () => {
 
   return (
     <button
-      className={`fixed bottom-4 right-4 bg-coral-red text-white py-4 px-4 rounded-full transition-opacity duration-300 ${
+      className={`fixed bottom-4 right-4 bg-coral-red text-white py-4 px-4 rounded-full transition-opacity duration-300 animate-bounce ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
       onClick={scrollToTop}
